f3157736d393e291e5d01053ee4053dc2429f49b
Cachary Tolentino <tolenti2@go.stockton.edu>
2024-10-05 15:40:50 -0400

    setup/initial-environment

---
+++ main.py
@@ -0,0 +1,41 @@
+#KDDM-Diabetes Dataset
+#Contributors: Cachary Tolentino and Ian Valiante
+#10/5/24
+
+#imports
+import matplotlib.pyplot as plt
+import numpy as np 
+import pandas as pd
+
+
+#Reading dataset
+df = pd.read_csv("diabetes_dataset00.csv")
+
+
+
+
+
+
+
+#Data Preprocessing 
+
+
+
+
+
+
+
+
+
+
+#Data Mining
+
+
+
+
+
+
+
+
+
+#Data Postprocessing
